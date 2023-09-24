a0a2e4e21ed6c21f024ce16b97ac0afa38ab54ed
Anshuman Solanki <anshumansolanki@Anshumans-MacBook-Air.local>
2023-09-24 13:05:18 +0530

    chore: remove unnecessary imports

--- src/app/MyForm.tsx
+++ src/app/MyForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import './MyForm.css'
 import FormInput from "./FormInput"
 import { useFormStateAndValidate } from "./hooks";
